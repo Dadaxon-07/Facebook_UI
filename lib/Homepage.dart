a5d4ca9721421c09c3b42d3b662cde223402082d
user <pragramist07@gmail.com>
2024-07-20 02:18:55 +0500

    Facebook addPost UI

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -43,7 +43,7 @@ class _HomepageState extends State<Homepage> {
                       Container(
                           padding: EdgeInsets.all(6),
                           height: 70,
-                          width: 300,
+                          width: 290,
                           decoration: BoxDecoration(
                               color: Colors.white,
                               border: Border.all(width: 1, color: Colors.grey),
@@ -160,10 +160,14 @@ class _HomepageState extends State<Homepage> {
                   ),
                 ),
                 Container(
-                  height: 400,
-                  child: ListView(
-                    children: [itemPost(img: "assets/img_6.png")],
-                  ),
+                  height: 550,
+                  child: itemPost(
+                      img: "assets/img_5.png",
+                      name: "User two ",
+                      title: "1 hour ago",
+                      description:
+                          "All the Lorem Ipsum generators on the internet tend yo repeat predifened",
+                      backimg: "assets/img_6.png"),
                 )
               ],
             ),
@@ -202,7 +206,7 @@ class _HomepageState extends State<Homepage> {
               style:
                   TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
             ),
-          )
+          ),
         ],
       ),
     );
@@ -217,10 +221,90 @@ class _HomepageState extends State<Homepage> {
     icon,
   }) {
     return Container(
-      height: 350,
-      color: Colors.red,
-      child: ListTile(
-      ),
-    );
+        color: Colors.white,
+        height: 550,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(15),
+                  height: 70,
+                  width: 70,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(img), fit: BoxFit.cover),
+                      shape: BoxShape.circle),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 25),
+                      child: Text(
+                        name,
+                        style: TextStyle(
+                            fontSize: 19, fontWeight: FontWeight.w900),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 5),
+                      child: Text(
+                        title,
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 160,
+                ),
+                Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: Icon(Icons.menu_rounded))
+              ],
+            ),
+            Container(
+              height: 55,
+                margin: EdgeInsets.all(8),
+                child: Text(
+                  description,
+                  style: TextStyle(fontSize: 16, color: Colors.black),
+                )),
+            Container(
+              height: 260,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(backimg), fit: BoxFit.cover)),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(margin: EdgeInsets.all(8), child: Row(
+                  children: [
+                    Icon(Icons.favorite, color: Colors.red,),
+                    Container(margin: EdgeInsets.all(5), child: Text("Like", style: TextStyle(color: Colors.red, fontWeight: FontWeight.w500),))
+                  ],
+                )),
+                Container(margin: EdgeInsets.all(8), child: Row(
+                  children: [
+                    Icon(Icons.comment,  color: Colors.blue[600],),
+                    Container(margin: EdgeInsets.all(5), child: Text("Comment", style: TextStyle(color: Colors.blue[800], fontWeight: FontWeight.w500),))
+                  ],
+                )),
+                Container(margin: EdgeInsets.all(8), child: Row(
+                  children: [
+                    Icon(Icons.share, color: Colors.grey[600],),
+                    Container(margin: EdgeInsets.all(12), child: Text("Share", style: TextStyle(color: Colors.grey[800], fontWeight: FontWeight.w500),))
+                  ],
+                )),
+              ],
+            )
+          ],
+        ));
   }
 }
